66aa01126732d54b9156f528f10d491abaa2ede7
Sangho Park <sangho1206.park@samsung.com>
2015-05-13 16:22:41 +0900

    Revert "Merge branch 'spin-develop' into tizen"
    This reverts commit 35eb64714feb7dc58affc0a8e9e74da4a8d20adc, reversing
    changes made to ff58b3c839a627da9d2e894df78fd8b699123e20.
    
    Change-Id: I178baeb2e834a0aa102e5d1296fd1b54edd4f44e
    Signed-off-by: Sangho Park <sangho1206.park@samsung.com>

--- filesystem/etc/profile.d/proxy_setting.sh
+++ filesystem/etc/profile.d/proxy_setting.sh
@@ -1,34 +1,27 @@
 #!/bin/sh
-if grep -q "http_proxy=" /proc/cmdline ; then
-        __proxy=`sed 's/^.*http_proxy=\([^, ]*\).*$/\1/g' /proc/cmdline`
-        if [ "x${__proxy}" = "x" ]; then
-            export "http_proxy="
-        else
-            export "http_proxy=http://${__proxy}/"
-        fi
-fi
-if grep -q "https_proxy=" /proc/cmdline ; then
-        __proxy=`sed 's/^.*https_proxy=\([^, ]*\).*$/\1/g' /proc/cmdline`
-        if [ "x${__proxy}" = "x" ]; then
-            export "https_proxy="
-        else
-            export "https_proxy=https://${__proxy}/"
-        fi
-fi
-if grep -q "ftp_proxy=" /proc/cmdline ; then
-        __proxy=`sed 's/^.*ftp_proxy=\([^, ]*\).*$/\1/g' /proc/cmdline`
-        if [ "x${__proxy}" = "x" ]; then
-            export "ftp_proxy="
-        else
-            export "ftp_proxy=ftp://${__proxy}/"
-        fi
-fi
-if grep -q "socks_proxy=" /proc/cmdline ; then
-        __proxy=`sed 's/^.*socks_proxy=\([^, ]*\).*$/\1/g' /proc/cmdline`
-        if [ "x${__proxy}" = "x" ]; then
-            export "socks_proxy="
-        else
-            export "socks_proxy=socks://${__proxy}/"
-        fi
-fi
-export "no_proxy=localhost,127.0.0.1/8,10.0.0.0/16"
+
+function cmd_get
+{
+	cat /proc/cmdline | tr -s [:space:] '\n' | grep -m1 "^$1=" | cut -d= -f2
+}
+
+__proxy=`cmd_get http_proxy`
+[ -n "${__proxy}" ] && export "http_proxy=http://${__proxy}/"
+
+__proxy=`cmd_get https_proxy`
+[ -n "${__proxy}" ] && export "https_proxy=https://${__proxy}/"
+
+__proxy=`cmd_get ftp_proxy`
+[ -n "${__proxy}" ] && export "ftp_proxy=ftp://${__proxy}/"
+
+__proxy=`cmd_get socks_proxy`
+[ -n "${__proxy}" ] && export "socks_proxy=socks://${__proxy}/"
+
+no_proxy="localhost,127.0.0.1/8,10.0.0.0/16"
+
+# Work around for bug in zypper, not handling netmasks in no_proxy.
+# Exlicitly add default gateway ip to no_proxy.
+__gw=`cmd_get ip | cut -d: -f3`
+[ -n "${__gw}" ] && no_proxy="${no_proxy},${__gw}"
+
+export no_proxy
