7f73533d6d5499462334f0e02e216a8aef447eb1
Sooyoung Ha <yoosah.ha@samsung.com>
2016-08-04 16:43:06 +0900

    script: update system info with model-config.xml
    Change-Id: I380fe24496a6543b3acec4e84a9422f08f4765b6
    Signed-off-by: Sooyoung Ha <yoosah.ha@samsung.com>

--- filesystem/etc/emulator/prerun.d/set-model-config.sh
+++ filesystem/etc/emulator/prerun.d/set-model-config.sh
@@ -28,19 +28,27 @@ if grep -q "video=" $CMDLINE ; then
             echo -e "- resolution value is non-integer argument"
         else
             WIDTH_KEY="tizen.org\/feature\/screen.width\" type=\"int\""
+            WIDTH_KEY_NAME="tizen.org/feature/screen.width"
             sed -i s/"$WIDTH_KEY".*\</"$WIDTH_KEY"\>"$WIDTH"\</ $XML
+            ${NEW_ROOT}/usr/bin/system_info_update_db -r ${NEW_ROOT} -k ${WIDTH_KEY_NAME} -t int -g platform -v ${WIDTH}
             HEIGHT_KEY="tizen.org\/feature\/screen.height\" type=\"int\""
+            HEIGHT_KEY_NAME="tizen.org/feature/screen.height"
             sed -i s/"$HEIGHT_KEY".*\</"$HEIGHT_KEY"\>"$HEIGHT"\</ $XML
+            ${NEW_ROOT}/usr/bin/system_info_update_db -r ${NEW_ROOT} -k ${HEIGHT_KEY_NAME} -t int -g platform -v ${HEIGHT}
             echo -e "- width=$WIDTH, height=$HEIGHT"
 
             # screen size
             SCREENSIZE_KEY="tizen.org\/feature\/screen.size"
             SCREENSIZE_KEY_NORMAL=""$SCREENSIZE_KEY".normal"
             SCREENSIZE_KEY_NORMAL_RESOLUTION=""$SCREENSIZE_KEY_NORMAL"."$WIDTH"."$HEIGHT"\" type=\"bool\""
+            SCREENSIZE_KEY_NORMAL_NAME="tizen.org/feature/screen.size.normal"
 
             sed -i s/"$SCREENSIZE_KEY_NORMAL".[0-9].*"type=\"bool\"".*true/"&!!!"/ $XML
+            TMP_RESOLUTION=`grep "true!!!" $XML | sed s/.*normal\.// | cut -d '"' -f1`
+            ${NEW_ROOT}/usr/bin/system_info_update_db -r ${NEW_ROOT} -k ${SCREENSIZE_KEY_NORMAL_NAME}.${TMP_RESOLUTION} -t bool -g platform -v false
             sed -i s/true!!!/false/ $XML
             sed -i s/"$SCREENSIZE_KEY_NORMAL_RESOLUTION".*\</"$SCREENSIZE_KEY_NORMAL_RESOLUTION"\>true\</ $XML
+            ${NEW_ROOT}/usr/bin/system_info_update_db -r ${NEW_ROOT} -k ${SCREENSIZE_KEY_NORMAL_NAME}.${WIDTH}.${HEIGHT} -t bool -g platform -v true
         fi
 fi
 
@@ -60,7 +68,9 @@ if grep -q "dpi=" $CMDLINE ; then
             fi
 
             DPI_KEY="tizen.org\/feature\/screen.dpi\" type=\"int\""
+            DPI_KEY_NAME="tizen.org/feature/screen.dpi"
             sed -i s/"$DPI_KEY".*\</"$DPI_KEY"\>"$SCREEN_DPI"\</ $XML
+            ${NEW_ROOT}/usr/bin/system_info_update_db -r ${NEW_ROOT} -k ${DPI_KEY_NAME} -t int -g platform -v ${SCREEN_DPI}
             echo -e "- dpi=$SCREEN_DPI"
         fi
 fi
